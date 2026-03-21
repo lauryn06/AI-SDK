9c2e6d774fb27afdb052d7a3d0845ae31fbc32b7
lauryn mwase <laurynmwase06@gmail.com>
2026-03-21 15:37:00 +0200

    embedded mistral ai, api key and the routing

--- ai/app/api/chat/route.ts
+++ ai/app/api/chat/route.ts
@@ -1,12 +1,13 @@
 // 
 import { streamText, UIMessage, convertToModelMessages } from 'ai';
-import { google } from '@ai-sdk/google';
+import { mistral } from '@ai-sdk/mistral';
+
 
 export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
 
   const result = streamText({
-    model: google('gemini-1.5-pro'), // ✅ Gemini here
+    model:  mistral('mistral-small-latest'),
     messages: await convertToModelMessages(messages),
   });
 
